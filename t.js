e2e54e7df75ad997650ecd96eeccb06ba08d2ca0
陈随易 <bimostyle@qq.com>
2025-10-24 17:45:55 +0800

    1

--- t.js
+++ t.js
@@ -4,7 +4,8 @@ const sql = new Bun.SQL({
     username: 'root2',
     password: 'root2',
     bigint: false
+});
 
 const [{ x }] = await sql`SELECT 10000000000000 as x`;
 
-console.log(typeof x, x); // "string" 10000000000000
+console.log(typeof x, x); // "string" 100000000000002
