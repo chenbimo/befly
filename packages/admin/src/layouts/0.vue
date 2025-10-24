fec28fb2f6b208ecce66aff3c2aabb576d60b103
陈随易 <bimostyle@qq.com>
2025-10-24 19:54:23 +0800

    1

--- packages/admin/src/layouts/0.vue
+++ packages/admin/src/layouts/0.vue
@@ -136,7 +136,7 @@ $Method.fetchUserMenus();
         display: flex;
         align-items: center;
         justify-content: space-between;
-        padding: 0 32px;
+        padding: 0 15px;
         background: #ffffff;
         border-radius: 8px;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.04);
