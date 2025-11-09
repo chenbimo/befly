a12e6593cd2ca498e39d97b89d5b2072f4de1ad4
陈随易 <bimostyle@qq.com>
2025-11-09 23:38:09 +0800

    1

--- packages/vite/config/index.ts
+++
@@ -1,9 +0,0 @@
-﻿/**
- * Vite 配置模块统一导出
- */
-export { plugins } from './plugins';
-export { viteResolve, define } from './alias';
-export { css } from './css';
-export { server, logLevel } from './server';
-export { build } from './build';
-export { optimizeDeps } from './optimize';
