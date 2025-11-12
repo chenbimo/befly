a2cf181dc371876d7dd21305f883d5ce1980d7e0
陈随易 <bimostyle@qq.com>
2025-11-12 13:28:46 +0800

    1

--- packages/addonAdmin/views/index/index.vue
+++ packages/addonAdmin/views/index/index.vue
@@ -25,5 +25,8 @@ import EnvironmentInfo from './components/environmentInfo.vue';
     height: 100%;
     background-color: #fff;
     padding: 15px;
+    border: 1px solid #e8eaed;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.04);
+    border-radius: 8px;
 }
 </style>
