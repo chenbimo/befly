a66021f1770053fe1a7b8aad2dd19d434a4c5685
陈随易 <bimostyle@qq.com>
2025-11-12 13:52:00 +0800

    1

--- packages/addonAdmin/apis/admin/list.ts
+++ packages/addonAdmin/apis/admin/list.ts
@@ -1,13 +1,8 @@
-﻿/**
- * 获取管理员列表
- */
-
-import { Yes } from 'befly';
+﻿import { Yes } from 'befly';
 
 export default {
     name: '获取管理员列表',
     handler: async (befly, ctx) => {
-        // 查询所有管理员（框架自动排除password字段，自动转换字段名为小驼峰）
         const result = await befly.db.getList({
             table: 'addon_admin_admin',
             page: ctx.body.page || 1,
