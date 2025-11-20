14e69a5de719c0af43332a6ba3b710fb1a66c73b
陈随易 <bimostyle@qq.com>
2025-11-20 22:10:52 +0800

    1

--- packages/util/dist/arrayToTree.d.ts
+++
@@ -1,8 +0,0 @@
-export interface ArrayToTreeOptions<T = any> {
-    idField?: string;
-    pidField?: string;
-    childrenField?: string;
-    rootPid?: any;
-    mapFn?: (node: T) => T;
-}
-export declare function arrayToTree<T = any>(items: T[], options?: ArrayToTreeOptions<T>): T[];
--- packages/util/dist/pickFields.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * 挑选指定字段
- */
-export declare const pickFields: <T extends Record<string, any>>(obj: T, keys: string[]) => Partial<T>;
