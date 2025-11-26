7a06cfa6c52afc8af7e2c6cef8893bdb64696b49
陈随易 <bimostyle@qq.com>
2025-11-27 01:18:02 +0800

    1

--- packages/util/types/fieldClear.d.ts
+++ packages/util/types/fieldClear.d.ts
@@ -5,7 +5,12 @@ export interface FieldClearOptions {
     excludeValues?: any[];
     keepMap?: Record<string, any>;
 }
-export type FieldClearResult<T> = T extends Array<infer U> ? Array<FieldClearResult<U>> : T extends object ? {
-    [K in keyof T]?: T[K];
-} : T;
+export type FieldClearResult<T> =
+    T extends Array<infer U>
+        ? Array<FieldClearResult<U>>
+        : T extends object
+          ? {
+                [K in keyof T]?: T[K];
+            }
+          : T;
 export declare function fieldClear<T = any>(data: T | T[], options?: FieldClearOptions): FieldClearResult<T>;
