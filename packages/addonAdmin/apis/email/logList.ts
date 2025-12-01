a10f85a47f01a45a4130a0b1aa64d3434b089589
陈随易 <bimostyle@qq.com>
2025-12-01 20:29:55 +0800

    1

--- packages/addonAdmin/apis/email/logList.ts
+++ packages/addonAdmin/apis/email/logList.ts
@@ -1,17 +1,11 @@
-import { Fields } from 'befly-core/config.js';
-
 export default {
     name: '邮件发送日志列表',
-    fields: {
-        page: Fields.page,
-        limit: Fields.limit
-    },
     handler: async (befly, ctx) => {
         const result = await befly.db.getList({
             table: 'addon_admin_email_log',
             page: ctx.body.page || 1,
             limit: ctx.body.limit || 30,
-            orderBy: { sendTime: 'desc' }
+            orderBy: ['sendTime#DESC']
         });
 
         return befly.tool.Yes('获取成功', result);
