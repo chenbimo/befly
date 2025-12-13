6e372bb6405ee040150e3d647a02b6f06a3131e5
陈随易 <bimostyle@qq.com>
2025-12-13 23:02:11 +0800

    1

--- packages/addonAdmin/apis/dict/detail.ts
+++ packages/addonAdmin/apis/dict/detail.ts
@@ -5,8 +5,25 @@
     handler: async (befly, ctx) => {
         const dict = await befly.db.getOne({
             table: 'addon_admin_dict',
-            joins: [{ table: 'addon_admin_dict_type', on: 'addon_admin_dict.type_code = addon_admin_dict_type.code' }],
-            fields: ['addon_admin_dict.id', 'addon_admin_dict.typeCode', 'addon_admin_dict.key', 'addon_admin_dict.label', 'addon_admin_dict.sort', 'addon_admin_dict.remark', 'addon_admin_dict.createdAt', 'addon_admin_dict.updatedAt', 'addon_admin_dict_type.name AS typeName'],
+            joins: [
+                //
+                {
+                    table: 'addon_admin_dict_type',
+                    on: 'addon_admin_dict.type_code = addon_admin_dict_type.code'
+                }
+            ],
+            fields: [
+                //
+                'addon_admin_dict.id',
+                'addon_admin_dict.typeCode',
+                'addon_admin_dict.key',
+                'addon_admin_dict.label',
+                'addon_admin_dict.sort',
+                'addon_admin_dict.remark',
+                'addon_admin_dict.createdAt',
+                'addon_admin_dict.updatedAt',
+                'addon_admin_dict_type.name AS typeName'
+            ],
             where: { 'addon_admin_dict.id': ctx.body.id }
         });
 
